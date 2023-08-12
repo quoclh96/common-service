9e490c0d793fa8f21ae7ac259f66135b388e6430
PC <PC@LeHuuQuoc>
2023-08-12 16:16:39 +0700

    update common service

--- common-service/src/main/java/service/CommonService.java
+++ common-service/src/main/java/service/CommonService.java
@@ -2,7 +2,11 @@ package service;
 
 public class CommonService {
 
-	public void commonService() {
-		
+	public static void main(String[] args) {
+		commonService();
+	}
+	public static void commonService() {
+		UsService us = new UsService();
+		us.usService();
 	}
 }
